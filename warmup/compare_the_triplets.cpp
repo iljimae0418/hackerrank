31539c3896386b95a9ce05bf0f0024f9b6f5c6dd
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2017-10-21 17:49:32 -0700

    Create compare_the_triplets.cpp

---
+++ warmup/compare_the_triplets.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int a[3],b[3]; 
+    for (int i = 0; i < 3; i++){
+        cin >> a[i];  
+    }
+    for (int i = 0; i < 3; i++){
+        cin >> b[i];  
+    }
+    int alice = 0, bob = 0; 
+    for (int i = 0; i < 3; i++){
+        if (a[i] > b[i]) alice++; 
+        else if (a[i] < b[i]) bob++; 
+    }    
+    cout << alice << " " << bob << endl; 
+    return 0;
+}
